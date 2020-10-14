b7a6f243160288a0de0d4c7406aa60aa81a33e81
iromashko <iromashko@me.com>
2020-10-14 14:10:14 +0300

    add: custom password validator

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
-import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
 
