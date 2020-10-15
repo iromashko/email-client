af5cbf1f066e6c455dcddcab9432e3972833b99b
iromashko <iromashko@me.com>
2020-10-15 07:09:35 +0300

    add: unique username validator

---
+++ src/app/auth/validators/unique-username.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AsyncValidator, FormControl } from '@angular/forms';
+import { of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UniqueUsername implements AsyncValidator {
+  constructor(private http: HttpClient) {}
+
+  validate = (control: FormControl) => {
+    const { value } = control;
+
+    return this.http
+      .post<any>('https://api.angular-email.com/auth/username', {
+        username: value,
+      })
+      .pipe(
+        map((value) => {
+          if (value.available) {
+            return null;
+          }
+        }),
+        catchError((err) => {
+          if (err.error.username) {
+            return of({ nonUniqueUsername: true });
+          } else {
+            return of({ noConnection: true });
+          }
+        })
+      );
+  };
+}
