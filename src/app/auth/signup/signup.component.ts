7ef30255c3b1269a12a041913b7c93b12684584f
iromashko <iromashko@me.com>
2021-01-08 05:31:48 +0300

    part 2

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatchPassword } from '../validators/match-password';
+import { UniqueUsername } from '../validators/unique-username';
 
 @Component({
   selector: 'app-signup',
@@ -11,12 +12,16 @@ import { MatchPassword } from '../validators/match-password';
 export class SignupComponent implements OnInit {
   authForm = new FormGroup(
     {
-      username: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(20),
-        Validators.pattern(/^[a-z0-9]+$/),
-      ]),
+      username: new FormControl(
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+          Validators.pattern(/^[a-z0-9]+$/),
+        ],
+        [this.uniqueUsername.validate]
+      ),
       password: new FormControl('', [
         Validators.required,
         Validators.minLength(4),
@@ -31,7 +36,10 @@ export class SignupComponent implements OnInit {
     { validators: [this.matchPassword.validate] }
   );
 
-  constructor(private matchPassword: MatchPassword) {}
+  constructor(
+    private matchPassword: MatchPassword,
+    private uniqueUsername: UniqueUsername
+  ) {}
 
   ngOnInit(): void {}
 }
