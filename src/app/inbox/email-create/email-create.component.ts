cfdbaa404dfd5fb793c17b8f60badf51c65ac031
iromashko <iromashko@me.com>
2021-01-13 05:27:52 +0300

    part 12

---
+++ src/app/inbox/email-create/email-create.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Email } from '../email';
+
+@Component({
+  selector: 'app-email-create',
+  templateUrl: './email-create.component.html',
+  styleUrls: ['./email-create.component.scss'],
+})
+export class EmailCreateComponent implements OnInit {
+  showModal = false;
+  email: Email;
+
+  constructor() {
+    this.email = {
+      id: '',
+      to: '',
+      subject: '',
+      html: '',
+      text: '',
+      from: 'iromashko@angular-email.com',
+    };
+  }
+
+  ngOnInit(): void {}
+}
---
+++ src/app/inbox/email-form/email-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Email } from '../email';
+
+@Component({
+  selector: 'app-email-form',
+  templateUrl: './email-form.component.html',
+  styleUrls: ['./email-form.component.scss'],
+})
+export class EmailFormComponent implements OnInit {
+  emailForm: FormGroup;
+  @Input() email: Email;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const { subject, from, to, text } = this.email;
+    this.emailForm = new FormGroup({
+      to: new FormControl(to, [Validators.required, Validators.email]),
+      from: new FormControl({value: from, disabled: true}),
+      subject: new FormControl(subject, [Validators.required]),
+      text: new FormControl(text, [Validators.required]),
+    });
+  }
+}
